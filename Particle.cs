ca794ae7511102e78d7f6c3930a8f43c95b272d6
RedLegend97 <61790422+RedLegend97@users.noreply.github.com>
2020-03-09 18:01:16 +0300

    Add files via upload

---
+++ Particle.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Physics2d
+{
+    public class Particle
+    {
+        /// <summary>
+        /// Gets or sets the damping of the particle.
+        /// </summary>
+        /// <remarks>
+        /// Damping represents a rough approximation of drag. A proportion of the
+        /// objects velocty is removed at each update based on this value. A value
+        /// of 1 means the object keeps all its velocity.
+        /// </remarks>
+        public float damping { get; set; }
+
+        /// <summary>
+        /// Gets or sets the position of the particle.
+        /// </summary>
+        public Vector2D position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the velocity of the particle.
+        /// </summary>
+        public Vector2D velocity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the accumulated force applied to the particle.
+        /// </summary>
+        protected Vector2D forceAccum { get; set; }
+
+        /// <summary>
+        /// Gets or sets the acceleration of the particle.
+        /// </summary>
+        public Vector2D acceleration { get; set; }
+
+        /// <summary>
+        /// Gets or sets the inverse mass of the particle.
+        /// </summary>
+        /// <remarks>
+        /// It is more practicle to use an inverse mass as integration is simpler
+        /// and it makes more sense to have an object of infinite mass i.e. on that
+        /// cannot be moved.
+        /// </remarks>
+        public float inverseMass { get; set; }
+
+        /// <summary>
+        /// Gets or sets the mass of the particle.
+        /// </summary>
+        public float mass
+        {
+            get
+            {
+                if (Equals(inverseMass, 0f))
+                {
+                    return float.MaxValue;
+                }
+                return 1.0f / inverseMass;
+            }
+
+            set
+            {
+                // Mass cannot be zero, since force will generate infinite acceleration.
+                if (Equals(value, 0f))
+                {
+                    throw new Exception("Mass cannot be zero");
+                }
+
+                inverseMass = 1.0f / value;
+            }
+
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Particle"/> class.
+        /// </summary>
+        public Particle()
+        {
+            position = new Vector2D();
+            velocity = new Vector2D();
+            forceAccum = new Vector2D();
+            acceleration = new Vector2D();
+        }
+
+        /// <summary>
+        /// Determines if this particle has finite mass.
+        /// </summary>
+        /// <returns><c>true</c> if particle has finite mass; otherwise, <c>false</c>.</returns>
+        public bool HasFiniteMass()
+        {
+            return inverseMass >= 0.0;
+        }
+
+        /// <summary>
+        /// Set the velocity of this particle.
+        /// </summary>
+        /// <param name="x">The X component.</param>
+        /// <param name="y">The Y component.</param>
+        /// <param name="z">The Z component.</param>
+        public void SetVelocity(float x, float y)
+        {
+            velocity.x = x;
+            velocity.y = y;
+        }
+
+        /// <summary>
+        /// Set the position of this particle.
+        /// </summary>
+        /// <param name="x">The X component.</param>
+        /// <param name="y">The Y component.</param>
+        /// <param name="z">The Z component.</param>
+        public void SetPosition(float x, float y)
+        {
+            position.x = x;
+            position.y = y;
+        }
+
+        /// <summary>
+        /// Set the acceleration of this particle.
+        /// </summary>
+        /// <param name="x">The X component.</param>
+        /// <param name="y">The Y component.</param>
+        /// <param name="z">The Z component.</param>
+        public void SetAcceleration(float x, float y)
+        {
+            acceleration.x = x;
+            acceleration.y = y;
+        }
+
+        /// <summary>
+        /// Get a copy of the particle position.
+        /// </summary>
+        /// <returns>A copy of the particle position vector.</returns>
+        public Vector2D GetPosition()
+        {
+            return new Vector2D(position.x, position.y);
+        }
+
+        /// <summary>
+        /// Get a copy of the particle velocity.
+        /// </summary>
+        /// <returns>A copy of the particle velocity vector.</returns>
+        public Vector2D GetVelocity()
+        {
+            return new Vector2D(velocity.x, velocity.y);
+        }
+
+        /// <summary>
+        /// Get a copy of the particle acceleration.
+        /// </summary>
+        /// <returns>A copy of the particle acceleration vector.</returns>
+        public Vector2D GetAcceleration()
+        {
+            return new Vector2D(acceleration.x, acceleration.y);
+        }
+
+        /// <summary>
+        /// Calculate the new position and velocity of the particle.
+        /// </summary>
+        /// <param name="duration">
+        /// Time interval over which to update the position and velocity.
+        /// This is currently the time between frames.
+        /// </param>
+        public void Integrate(float duration)
+        {
+            // We don't integrate things with zero mass.
+            if (inverseMass <= 0.0f)
+            {
+                return;
+            }
+
+            // Make sure duration is positive.
+            if (duration <= 0.0)
+            {
+                throw new ArgumentOutOfRangeException("duration", "must be greater than 0");
+            }
+
+            // Update linear position.
+            position.AddScaledVector(velocity, duration);
+
+            // Work out the acceleration from the force.
+            Vector2D resultingAcc = GetAcceleration();
+            resultingAcc.AddScaledVector(forceAccum, inverseMass);
+
+            // Update linear velocity from the acceleration.
+            velocity.AddScaledVector(resultingAcc, duration);
+
+            // Impose drag.
+            velocity *= Mathf.Pow(damping, duration);
+
+            // Clear the forces.
+            ClearAccumulator();
+        }
+
+        /// <summary>
+        /// Add a force to this particle.
+        /// </summary>
+        /// <param name="force">The force to add.</param>
+        public void AddForce(Vector2D force)
+        {
+            force += force;
+        }
+
+        /// <summary>
+        /// Clear the accumulated force.
+        /// </summary>
+        private void ClearAccumulator()
+        {
+            forceAccum.Clear();
+        }
+    }
+}
+
+
+
--- Vector2D.cs
+++ Vector2D.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
-namespace Physics2D
+namespace Physics2d
 {
     public class Vector2D
     {
@@ -19,14 +21,95 @@ namespace Physics2D
         }
 
         //Vector2D Constructor
-        public Vector2D(float a, float b)
+        public Vector2D(float x, float y)
         {
-            x = a;
-            y = b;
+            this.x = x;
+            this.y = y;
+        }
+
+        /// <summary>
+        /// Creates an instance of the <see cref="Vector3"/> class.
+        /// </summary>
+        /// <param name="vector">The vector to copy.</param>
+        public Vector2D(Vector2D vector)
+        {
+            x = vector.x;
+            y = vector.y;
+        }
+        private static readonly Vector2D zeroVector = new Vector2D(0.0f, 0.0f);
+        private static readonly Vector2D oneVector = new Vector2D(1f, 1f);
+        private static readonly Vector2D upVector = new Vector2D(0.0f, 1f);
+        private static readonly Vector2D downVector = new Vector2D(0.0f, -1f);
+        private static readonly Vector2D leftVector = new Vector2D(-1f, 0.0f);
+        private static readonly Vector2D rightVector = new Vector2D(1f, 0.0f);
+        /// <summary>
+        ///   <para>Shorthand for writing Vector2(0, 0).</para>
+        /// </summary>
+        public static Vector2D zero
+        {
+            get
+            {
+                return Vector2D.zeroVector;
+            }
+        }
+
+        /// <summary>
+        ///   <para>Shorthand for writing Vector2(1, 1).</para>
+        /// </summary>
+        public static Vector2D one
+        {
+            get
+            {
+                return Vector2D.oneVector;
+            }
+        }
+
+        /// <summary>
+        ///   <para>Shorthand for writing Vector2(0, 1).</para>
+        /// </summary>
+        public static Vector2D up
+        {
+            get
+            {
+                return Vector2D.upVector;
+            }
+        }
+
+        /// <summary>
+        ///   <para>Shorthand for writing Vector2(0, -1).</para>
+        /// </summary>
+        public static Vector2D down
+        {
+            get
+            {
+                return Vector2D.downVector;
+            }
+        }
+
+        /// <summary>
+        ///   <para>Shorthand for writing Vector2(-1, 0).</para>
+        /// </summary>
+        public static Vector2D left
+        {
+            get
+            {
+                return Vector2D.leftVector;
+            }
+        }
+
+        /// <summary>
+        ///   <para>Shorthand for writing Vector2(1, 0).</para>
+        /// </summary>
+        public static Vector2D right
+        {
+            get
+            {
+                return Vector2D.rightVector;
+            }
         }
 
         //Invert function for Vector2D
-        void InvertVector2D()
+        public void InvertVector2D()
         {
             x = -x;
             y = -y;
@@ -38,15 +121,6 @@ namespace Physics2D
         public float MagnitudeVector2D
         {
             get { return Mathf.Sqrt(x * x + y * y); }
-        }
-       
-
-        /// <summary>
-        /// Gets the square of the magnitude.
-        /// </summary>
-        public float SquareMagnitudeVector2D
-        {
-            get { return x * x + y * y; }
         }
 
         /// <summary>
@@ -63,23 +137,70 @@ namespace Physics2D
         }
 
 
-
-        //void Vector2D operator *(float value)
-        //{
-        //    x *= value;
-        //    y * = value;
-        //}
-
-        //TODO TAKE A LOOK AT THIS FUNCTION AGAIN book page 26
         /// <summary>
-        /// Scale a vector by an amount.
+        /// Gets the square of the magnitude.
         /// </summary>
-        /// <param name="lhs">The left vector.</param>
-        /// <param name="value">The value to multiply the vector by.</param>
-        /// <returns>The resulting vector from the multiplication.</returns>
-        public static Vector2D operator *(Vector2D lhs, float value)
+        public float SquareMagnitudeVector2D
         {
-            return new Vector2D(lhs.x * value, lhs.y * value);
+            get { return x * x + y * y; }
+        }
+
+        //addition between two vectors and a float value
+        public static Vector2D operator +(Vector2D a, Vector2D b)
+        {
+            return new Vector2D(a.x + b.x, a.y + b.y);
+        }
+        public static Vector2D operator +(Vector2D a, float b)
+        {
+            return new Vector2D(a.x + b, a.y + b);
+        }
+        public static Vector2D operator +(float b, Vector2D a)
+        {
+            return new Vector2D(a.x + b, a.y + b);
+        }
+
+        //Substraction between vectors and float value
+        public static Vector2D operator -(Vector2D a, Vector2D b)
+        {
+            return new Vector2D(a.x - b.x, a.y - b.y);
+        }
+        public static Vector2D operator -(Vector2D a, float b)
+        {
+            return new Vector2D(a.x - b, a.y - b);
+        }
+        public static Vector2D operator -(float b, Vector2D a)
+        {
+            return new Vector2D(b - a.x, b - a.y);
+        }
+        //inverting a vector2d
+        public static Vector2D operator -(Vector2D a)
+        {
+            return new Vector2D(-a.x, -a.y);
+        }
+        //TODO NAME THIS PROPERLY
+        //TODO NAME THIS PROPERLY
+        //Dot product
+        public static Vector2D operator *(Vector2D a, Vector2D b)
+        {
+            return new Vector2D(a.x * b.x, a.y * b.y);
+        }
+        //Scale the vector by an amount
+        public static Vector2D operator *(Vector2D a, float d)
+        {
+            return new Vector2D(a.x * d, a.y * d);
+        }
+        public static Vector2D operator *(float d, Vector2D a)
+        {
+            return new Vector2D(a.x * d, a.y * d);
+        }
+        //Division
+        public static Vector2D operator /(Vector2D a, Vector2D b)
+        {
+            return new Vector2D(a.x / b.x, a.y / b.y);
+        }
+        public static Vector2D operator /(Vector2D a, float d)
+        {
+            return new Vector2D(a.x / d, a.y / d);
         }
 
         /// <summary>
@@ -91,6 +212,57 @@ namespace Physics2D
         {
             x += vector.x * scale;
             y += vector.y * scale;
+        }
+
+        /// <summary>
+        /// Calculate the component product.
+        /// </summary>
+        /// <param name="vector">The vector.</param>
+        /// <returns>A new vector representing the component product.</returns>
+        public Vector2D ComponentProduct(Vector2D vector)
+        {
+            return new Vector2D(x * vector.x, y * vector.y);
+        }
+
+        /// <summary>
+        /// Calculate the component product and update this vector with the result.
+        /// </summary>
+        /// <param name="vector">The vector.</param>
+        public void ComponentProductUpdate(Vector2D vector)
+        {
+            x *= vector.x;
+            y *= vector.y;
+        }
+
+        //TODO CROSS PRODUCT HAKKINDA KARAR VER ARAŞTIR
+
+        /// <summary>
+        /// Calculate the dot product.
+        /// </summary>
+        /// <param name="vector">The vector.</param>
+        /// <returns>The dot product.</returns>
+        public double ScalarProduct(Vector2D vector)
+        {
+            return (x * vector.x) + (y * vector.y);
+        }
+
+        /// <summary>
+        /// Clear this vector to (0, 0).
+        /// </summary>
+        public void Clear()
+        {
+            x = y = 0;
+        }
+    }
+
+
+    //TODO Testler
+    public class Test
+    {
+        public void test()
+        {
+            Vector2D x = new Vector2D();
+
         }
     }
 }
